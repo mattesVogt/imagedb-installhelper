9a89bfc7b83dc4de16d28a8b5dcdabcf7d5586ae
Matthias Vogt <m.vogt@4net.de>
2019-05-14 16:15:02 +0200

    Create Plugin.php

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Fournet\ImagedbBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Fournet\ImagedbBundle\FournetImagedbBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(FournetImagedbBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+
+}
